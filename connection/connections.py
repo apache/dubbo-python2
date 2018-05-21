cb95112aad2936492e91270aeb1c410c8128278a
侯锐 <hourui@qianmi.com>
2018-05-21 16:08:01 +0800

    重构了与网络连接相关的代码

--- connection/connections.py
+++ connection/connections.py
@@ -1,73 +1,118 @@
 # -*- coding: utf-8 -*-
 import re
 import socket
+import threading
 from random import randint
 from urllib import unquote
 
+import time
 from kazoo.client import KazooClient
 
 DUBBO_ZK_PROVIDERS = '/dubbo/{0}/providers'
 PROVIDER_HOST_PATTERN = re.compile(r'dubbo://(?P<host>[\d\D]+?)/')
 
+# 根据远程host保存与此host相关的连接
+connection_pool = {}
 
-def parse_provider_host(provider):
-    provider = unquote(provider)
-    m = PROVIDER_HOST_PATTERN.match(provider)
-    if m:
-        host = m.group('host')
+
+def scanning_invalid_connection():
+    """
+    清理已关闭的链接或超时的链接
+    :return:
+    """
+    while 1:
+        for key in connection_pool.keys():
+            conn = connection_pool[key]
+            if time.time() - conn.last_active > 10:
+                conn.close()
+            if conn.closed:
+                print 'close connection to remote host {}'.format(key)
+                del connection_pool[key]
+        time.sleep(5)
+
+
+thread = threading.Thread(target=scanning_invalid_connection)
+thread.start()
+
+
+def get_provider_connection(host):
+    """
+    通过host获取到与此host相关的socket，本地会对socket进行缓存
+    :param host:
+    :return:
+    """
+    if not connection_pool.get(host):
         ip, port = host.split(':')
-        return Client(ip, int(port))
-    raise ValueError('can\'t find provider host in [{0}]'.format(provider))
+        connection_pool[host] = Connection(ip, int(port))
+    return connection_pool[host]
 
 
-class ProvidersConnection(object):
+class ZkRegister(object):
     def __init__(self, hosts):
         zk = KazooClient(hosts=hosts)
         zk.start()
 
         self.zk = zk
-        self.connections = {}
+        self.hosts = {}
 
-    def get_connection(self, interface):
-        if self.connections.get(interface):
-            providers = self.connections[interface]
-        else:
+    def get_provider_host(self, interface):
+        """
+        从zk中可以根据接口名称获取到此接口某个provider的host
+        :param interface:
+        :return:
+        """
+        if not self.hosts.get(interface):
             path = DUBBO_ZK_PROVIDERS.format(interface)
             if self.zk.exists(path):
                 providers = self.zk.get_children(path)
-                providers = map(parse_provider_host, providers)
-                self.connections[interface] = providers
+                self._register_consumer(providers)
+                self.hosts[interface] = map(parse_provider_host, providers)
             else:
                 raise Exception('can\'t providers for interface {0}'.format(interface))
-        return providers[randint(0, len(providers) - 1)]
+        hosts = self.hosts[interface]
+        return hosts[randint(0, len(hosts) - 1)]
+
+    def _register_consumer(self, providers):
+        pass
 
     def close(self):
         self.zk.stop()
-        # 关闭所有与provider的连接
-        for interface in self.connections.keys():
-            providers = self.connections[interface]
-            for provider in providers:
-                provider.close()
 
 
-class Client(object):
+class Connection(object):
+    """
+    对Socket链接做了一些封装
+    """
+
     def __init__(self, host, port):
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.connect((host, port))
         self.__sock = sock
 
+        self.last_active = time.time()
+        self.closed = False
+
     def write(self, data):
+        self.last_active = time.time()
         self.__sock.sendall(data)
 
     def read(self, length):
+        self.last_active = time.time()
         return bytearray(self.__sock.recv(length))
 
     def close(self):
+        self.closed = True
         self.__sock.close()
 
 
-connection = ProvidersConnection('127.0.0.1:2181')
-
-if __name__ == '__main__':
-    print connection.get_connection('me.hourui.echo.provider.Echo')
-    print connection.get_connection('me.hourui.echo.provider.Echo')
+def parse_provider_host(provider):
+    """
+    从dubbo在zk中provider的信息中解析出此provider的host
+    :param provider:
+    :return:
+    """
+    provider = unquote(provider)
+    m = PROVIDER_HOST_PATTERN.match(provider)
+    if m:
+        return m.group('host')
+    raise ValueError('can\'t find provider host in [{0}]'.format(provider))
--- dubbo.py
+++ dubbo.py
@@ -3,28 +3,19 @@ import json
 
 from codec.decoder import get_response_body_length, Response
 from codec.encoder import encode
-from connection.connections import connection
+from connection.connections import ZkRegister, get_provider_connection
 
 
 class DubboClient(object):
-    class _Method(object):
-        def __init__(self, client_instance, method):
-            self.client_instance = client_instance
-            self.method = method
-
-        def __call__(self, *args, **kwargs):
-            return self.client_instance.call(self.method, *args, **kwargs)
-
-    def __init__(self, interface, version='1.0.0', dubbo_version='2.4.10'):
+    def __init__(self, zk_register, interface, version='1.0.0', dubbo_version='2.4.10'):
+        self.zk_register = zk_register
         self.interface = interface
         self.version = version
         self.dubbo_version = dubbo_version
 
-    def __getattr__(self, method, *args, **kwargs):
-        return self._Method(client_instance=self, method=method)
-
-    def call(self, method, *args):
-        client = connection.get_connection(self.interface)
+    def call(self, method, args=()):
+        host = self.zk_register.get_provider_host(self.interface)
+        client = get_provider_connection(host)
 
         request_param = {
             'dubbo_version': self.dubbo_version,
@@ -51,32 +42,34 @@ def pretty_print(value):
 
 
 if __name__ == '__main__':
-    dubbo = DubboClient('me.hourui.echo.provider.Echo')
-    pretty_print(dubbo.echo('张老师', '三', 19, 2000.0, True))
-    pretty_print(dubbo.echo1('昊天金阙无上至尊自然妙有弥罗至真高天上圣大慈仁者玉皇赦罪锡福大天尊玄穹高上帝'))
-    pretty_print(dubbo.echo2(False))
-    pretty_print(dubbo.echo3(1000000000, 0x7ff, 100000, 10000))
-    pretty_print(dubbo.echo4(1.00000004, 100000.0, 1.0, 2.0, 0.0))
-    assert 200 == dubbo.echo5(200)
-    assert 10000 == dubbo.echo5(10000)
+    zk = ZkRegister('127.0.0.1:2181')
+    dubbo = DubboClient(zk, 'me.hourui.echo.provider.Echo')
 
-    assert 0.0 == dubbo.echo6(0.0)
-    assert 1.0 == dubbo.echo6(1.0)
-    assert 100.0 == dubbo.echo6(100.0)
-    assert 100000.0 == dubbo.echo6(100000.0)
+    pretty_print(dubbo.call('echo', ['张老师', '三', 19, 2000.0, True]))
+    pretty_print(dubbo.call('echo1', '昊天金阙无上至尊自然妙有弥罗至真高天上圣大慈仁者玉皇赦罪锡福大天尊玄穹高上帝'))
+    pretty_print(dubbo.call('echo2', False))
+    pretty_print(dubbo.call('echo3', [1000000000, 0x7ff, 100000, 10000]))
+    pretty_print(dubbo.call('echo4', [1.00000004, 100000.0, 1.0, 2.0, 0.0]))
+    assert 200 == dubbo.call('echo5', 200)
+    assert 10000 == dubbo.call('echo5', 10000)
 
-    assert 10000000000 == dubbo.echo7(10000000000)
-    assert 0 == dubbo.echo7(0)
-    assert 100 == dubbo.echo7(100)
-    assert 1000 == dubbo.echo7(1000)
-    assert 100000 == dubbo.echo7(100000)
+    assert 0.0 == dubbo.call('echo6', 0.0)
+    assert 1.0 == dubbo.call('echo6', 1.0)
+    assert 100.0 == dubbo.call('echo6', 100.0)
+    assert 100000.0 == dubbo.call('echo6', 100000.0)
 
-    pretty_print(dubbo.echo8())
-    pretty_print(dubbo.echo9())
-    pretty_print(dubbo.echo10())
-    pretty_print(dubbo.echo11())
-    pretty_print(dubbo.echo12())
-    pretty_print(dubbo.echo13())
-    pretty_print(dubbo.echo14())
-    pretty_print(dubbo.echo15())
-    pretty_print(dubbo.echo16())
+    assert 10000000000 == dubbo.call('echo7', 10000000000)
+    assert 0 == dubbo.call('echo7', 0)
+    assert 100 == dubbo.call('echo7', 100)
+    assert 1000 == dubbo.call('echo7', 1000)
+    assert 100000 == dubbo.call('echo7', 100000)
+
+    pretty_print(dubbo.call('echo8'))
+    pretty_print(dubbo.call('echo9'))
+    pretty_print(dubbo.call('echo10'))
+    pretty_print(dubbo.call('echo11'))
+    pretty_print(dubbo.call('echo12'))
+    pretty_print(dubbo.call('echo13'))
+    pretty_print(dubbo.call('echo14'))
+    pretty_print(dubbo.call('echo15'))
+    pretty_print(dubbo.call('echo16'))
