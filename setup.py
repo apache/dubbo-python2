470cf7d17b06831c19c2c150f96aa0abf0743152
侯锐 <hourui@qianmi.com>
2018-07-19 23:04:38 +0800

    把socket转为非阻塞

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='python-dubbo',
-    version='0.0.3-beta13',
+    version='0.0.3-beta14',
     url='https://github.com/RitterHou/python-dubbo',
     author='hourui',
     author_email='hourui@qianmi.com',
