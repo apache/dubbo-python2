4c75398746ca68558abd1a6de9b37057bb2492cd
侯锐 <hourui@qianmi.com>
2018-07-20 16:59:23 +0800

    更新版本

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='python-dubbo',
-    version='0.0.3-beta14',
+    version='0.0.3-beta15',
     url='https://github.com/RitterHou/python-dubbo',
     author='hourui',
     author_email='hourui@qianmi.com',
